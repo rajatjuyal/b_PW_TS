54b9f0d0eceeefa985ad5f2981f2af05748fdab4
rajatjuyal <rajat.juyal@gmail.com>
2025-05-02 03:16:02 +0530

    udpated the playwright.config.ts to headless:true

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 0 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 5,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -39,7 +39,7 @@ export default defineConfig({
     // sloMo: 500,
     launchOptions:{slowMo:500},
     testIdAttribute: 'data-tab-item',
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
